35d413485055cda18835172b6d9066a9731abd18
Rehan khan <143535840+Rehankhan008@users.noreply.github.com>
2024-07-16 12:12:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,6 @@ app.get('/api/get/user', (req,res) => {
     res.send({name : 'Dev', email: 'dev@gmail.com', 'role': 'Developer'});
 })
 
-app.listen(9000, (req,res) => {
-    console.log('server running on port 9000')
+app.listen(80, (req,res) => {
+    console.log('server running on port 80')
 })
